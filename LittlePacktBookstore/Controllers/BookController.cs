425e1a8ae115f323c011526be588dff6d20edc3e
Laurie Roy <laurieroy.dev@gmail.com>
2020-12-12 16:21:09 -0700

    Add project files.

---
+++ LittlePacktBookstore/Controllers/BookController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LittlePacktBookstore.Models;
+using LittlePacktBookstore.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LittlePacktBookstore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly IRepository<Book> _bookRepository;
+        private readonly ILogger<BookController> _logger;
+
+        // GET: api/<BookController>
+        public BookController(IRepository<Book> bookRepository, ILogger<BookController> logger)
+        {
+            _bookRepository = bookRepository;
+            _logger = logger;
+        }
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<BookController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<BookController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<BookController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<BookController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ LittlePacktBookstore/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LittlePacktBookstore.Data;
+using LittlePacktBookstore.Models;
+using LittlePacktBookstore.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LittlePacktBookstore
+{
+    public class Startup
+    {
+		private IConfiguration _configuration;
+
+		public Startup(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+			services.AddMvc();
+			services.AddDbContext<LittlePacktBookStoreDbContex>(dbContextOptionBuilder =>
+			dbContextOptionBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+			services.AddScoped<IRepository<Book>, SqlBooksRepository>();
+			services.AddScoped<IRepository<Carousel>, SqlCarouselsRepository>();
+			services.AddScoped<IRepository<Order>, SqlOrdersRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+			app.UseMvc(ConfigureRoutes);			
+			app.UseStaticFiles();
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+        }
+
+		private static void ConfigureRoutes(IRouteBuilder routes)
+		{
+			routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+		}
+
+	}
+}
